76765551be95882fc2b023c01652e08b7f653743
brunoloubraz <brunolou.braz@gmail.com>
2024-04-10 15:31:55 -0300

    conteudo

---
+++ 3_secao03/dia03_api_rest_com_spring/aula/pod/src/test/java/com/podcasts/pod/PodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podcasts.pod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
